32c230eeb33eb74e5d0deed154049f293f78345c
Zoro-29 <zorotheuchiha01@gmail.com>
2026-08-26 00:17:13 +0530

    Added C practice files

---
+++ print1-Nnum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main (){
+    int n;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+    int i=0;
+    while(i<=n) {
+      i++;
+      printf("%d\n",i);
+    }
+    
+    
+
+
+    return 0;
+
+
+}
