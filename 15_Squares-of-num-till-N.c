7a37a7c69b98fe4e9988f2461d18138f80b26b84
Zoro-29 <zorotheuchiha01@gmail.com>
2026-08-26 23:24:54 +0530

    15 C code Prints squares till the N number

---
+++ 15_Squares-of-num-till-N.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+    int n;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+
+    int i=1;
+    while (i<=n) {
+      printf("%d\n", i*i);
+      i++;
+    }
+   
+    return 0;
+
+
+}
