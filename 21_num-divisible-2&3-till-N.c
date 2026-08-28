371584c842c4bca5ec2d3c312fbaf81b14eb87f6
Zoro-29 <zorotheuchiha01@gmail.com>
2026-08-28 23:12:12 +0530

    21 C code prints the number which are divisible by 2 and 3 till the N

---
+++ 21_num-divisible-2&3-till-N.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main (){
+    int n;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+
+    int i=1;
+
+    while (i<=n) {
+      if (i%2==0 && i%3==0) {
+        printf("%d\n",i);
+      }
+      i++;
+    }
+   
+    return 0;
+
+
+}
