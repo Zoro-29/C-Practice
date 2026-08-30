c4be186bed555505d41ad69a4420f32051824eac
Zoro-29 <zorotheuchiha01@gmail.com>
2026-08-30 21:13:22 +0530

    24 C code prints numbers 1-10 except multiples of 3

---
+++ 24_1-10-except-3-multiples.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main (){
+    int n;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+
+    int i=1;
+   
+
+    while(i<=n){
+      if(i%3!=0) {
+        printf("%d\n",i);
+        
+      }
+      i++;
+    }
+   
+    return 0;
+
+
+}
