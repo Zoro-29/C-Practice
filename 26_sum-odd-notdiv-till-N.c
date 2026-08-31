8c83caa4d2423931b9e0bb92f3690f57d24a5dbb
Zoro-29 <zorotheuchiha01@gmail.com>
2026-08-31 22:23:41 +0530

    26 C code  It calculates the sum of odd numbers up to n that are not divisible by 5.

---
+++ 26_sum-odd-notdiv-till-N.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main (){
+    int n;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+
+    int i=1;
+   int sum=0;
+
+    while(i<=n){
+      if(i%2!=0 && i%5!=0) {
+        sum+=i;
+        
+      }
+      i++;
+    }
+    printf("%d\n", sum);
+   
+    return 0;
+
+
+}
