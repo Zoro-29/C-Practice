f8c57f5c6e6385b1551e793048024a7287c7c48b
Zoro-29 <zorotheuchiha01@gmail.com>
2026-09-08 23:48:13 +0530

    34 C code Sum of the numbers individually and then prints it

---
+++ 34_sum-of-numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main (){
+    int n;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+
+   int sum=0;
+   int digit;
+
+   while(n!=0) {
+    digit=n%10;
+    n=n/10;
+    if(digit%2==0){
+      sum=sum+digit;
+    }
+   }
+   printf("The sum is : %d\n",sum);
+
+
+    return 0;
+
+
+}
